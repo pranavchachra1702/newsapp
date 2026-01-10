da8367d07e049995aa4c41ad18b2feaf011b05b4
pranavchachra1702 <pranavchachra1702@gmail.com>
2026-01-10 21:50:10 +0530

    News APP

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='background text-light pt-2 pb-5 text-center'>
+      copyright@mynewsapp.com
+    </footer>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,42 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 
 export default function Navbar() {
+    let [search, setSearch] = useState("")
+    let [searchParams] = useSearchParams()
+    let [q, setQ] = useState("")
+    let [language, setLanguage] = useState("")
+
+    let navigate = useNavigate()
+    function postSearch(e){
+        e.preventDefault()
+        navigate(`/?q=${search}&language=${language}`)
+        setSearch("")
+    }
+    useEffect(()=>{
+        setQ(searchParams.get("q")??"All")
+        setLanguage(searchParams.get("language")??"hi")
+    },[searchParams])
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg background sticky-top">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <Link className="navbar-brand text-light" to={`/?q=All&language=${language}`}>NewsApp</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link text-light active" aria-current="page" to={`/?q=All&language=${language}`}>Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <Link className="nav-link text-light" to={`/?q=Politics&language=${language}`}>Politics</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-light" to={`/?q=Crime&language=${language}`}>Crime</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-light" to={`/?q=Education&language=${language}`}>Education</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-light" to={`/?q=Science&language=${language}`}>Science</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-light" to={`/?q=Technology&language=${language}`}>Technology</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-light" to={`/?q=Economics&language=${language}`}>Economics</Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <Link className="nav-link text-light dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown
-                            </a>
+                            </Link>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider"/></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                <li><Link className="dropdown-item" to={`/?q=Entertainment&language=${language}`}>Entertainment</Link></li>
+                                <li><Link className="dropdown-item" to={`/?q=Geography&language=${language}`}>Geography</Link></li>
+                                <li><Link className="dropdown-item" to={`/?q=World&language=${language}`}>World</Link></li>
+                                <li><Link className="dropdown-item" to={`/?q=India&language=${language}`}>India</Link></li>
+                                <li><Link className="dropdown-item" to={`/?q=Cricket&language=${language}`}>Cricket</Link></li>
+                                <li><Link className="dropdown-item" to={`/?q=Sports&language=${language}`}>Sports</Link></li>
+                                <li><Link className="dropdown-item" to={`/?q=Jokes&language=${language}`}>Jokes</Link></li>
                             </ul>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link text-light dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Language
+                            </Link>
+                            <ul className="dropdown-menu">
+                                <li><Link className="dropdown-item" to={`/?q=${q}&language=hi`}>Hindi</Link></li>
+                                <li><Link className="dropdown-item" to={`/?q=${q}&language=en`}>English</Link></li>
+                            </ul>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    <form className="d-flex" role="search" onSubmit={postSearch}>
+                        <input className="form-control me-2" type="search" name='search' value={search} onChange={(e)=>setSearch(e.target.value)} placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-light" type="submit">Search</button>
                     </form>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
